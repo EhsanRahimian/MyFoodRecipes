f7a82a767ce57aa762137a199651a24c5d3444f4
Ethan Rahimian <nicootech@gmail.com>
2021-06-22 14:23:42 -0700

    Executors for Background Tasks

--- app/src/main/java/com/nicootech/myfoodrecipes/requests/RecipeApiClient.java
+++ app/src/main/java/com/nicootech/myfoodrecipes/requests/RecipeApiClient.java
@@ -1,11 +1,16 @@
 package com.nicootech.myfoodrecipes.requests;
 
+import com.nicootech.myfoodrecipes.AppExecutors;
 import com.nicootech.myfoodrecipes.models.Recipe;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+
+import static com.nicootech.myfoodrecipes.util.Constants.NETWORK_TIMEOUT;
 
 public class RecipeApiClient {
 
@@ -25,5 +30,21 @@ public class RecipeApiClient {
     }
     public LiveData<List<Recipe>> getRecipes(){
         return mRecipes;
+    }
+    public void searchRecipeApi(){
+        final Future handler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+                // retrieve dada from rest api
+                //mRecipes.postValue();
+            }
+        });
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                //let user know its timed out
+                handler.cancel(true);
+            }
+        }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
     }
 }
